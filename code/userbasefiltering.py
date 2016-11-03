2a7360531f6f4c1f05c907419912c0a9ec8c95d6
Jashwanth Reddy Madem <jswt001@gmail.com>
2016-11-03 00:18:06 -0700

    changed to item based

--- code/userbasefiltering.py
+++ code/userbasefiltering.py
@@ -1,11 +1,12 @@
-import numpy as np
-from sklearn.neighbors import NearestNeighbors
-from sklearn.metrics.pairwise import cosine_similarity
-from scipy.spatial import distance 
 import pdb
 import warnings
+import numpy as np
+import pickle
 from scipy import sparse
-import cPickle as pickle
+from scipy.spatial import distance 
+from sklearn.neighbors import NearestNeighbors
+from sklearn.metrics.pairwise import cosine_similarity
+
 
 warnings.filterwarnings('error')
 
@@ -33,9 +34,34 @@ def getUserProfiles(userData, questionData, trainData, qList, wordvec):
 		if np.linalg.norm(userProfiles[user]) == 0:
 			del userProfiles[user]
 		
-
 	return userProfiles
 
+
+def getItemProfiles(userData, questionData, trainData, uList, wordvec):
+	# userData dict userId -> list[topics]
+	# questionData dict quId -> topic (int)
+	# trainData list of tuples (quId, userId, 0/1)
+	# uList - list of uids (to maintain order)
+	# return itemProfiles dict userId -> numpy array [features (int -2 to 2)]
+	itemProfiles = {}
+	len_user = len(userData)
+	for ques in questionData:
+		itemProfiles[ques] = np.zeros(shape=(len_user, ))
+	for tup in trainData:
+		qid, uid, ans = tup
+		if userData[uid] in questionData[qid]:
+			val = 1
+		else:
+			val = 1
+		if ans == 0:
+			val = -1*val
+		itemProfiles[qid][uList.index(uid)] = val
+	for item in itemProfiles.keys():
+		#userProfiles[user] = np.hstack((userProfiles[user], wordvec[user]))
+		if np.linalg.norm(itemProfiles[item]) == 0:
+			del itemProfiles[item]
+		
+	return itemProfiles
 	
 
 def getKNearestUsers(userProfiles, k):
@@ -51,7 +77,6 @@ def getKNearestUsers(userProfiles, k):
 	A_sparse = sparse.csr_matrix(samples)
 	similarities = cosine_similarity(A_sparse)
 	#pdb.set_trace()
-
 
 	#cnt = 0
 	nb = {}
@@ -72,6 +97,8 @@ def getKNearestUsers(userProfiles, k):
 
 	#neigh.fit(samples)
 	return nb, uList
+
+
 def getScores(neighbors, uList, userProfiles, valData, qList):
 	scores = []
 	for val in valData:
@@ -98,43 +125,46 @@ def loadData():
 	valData = []
 	wordvec = {}
 	userorder = []
-	with open('user_info.txt', 'r') as f1:
+	with open('../train_data/user_info.txt', 'r') as f1:
 		for line in f1:
 			sp = line.split()
 			uid = sp[0]
 			userorder.append(uid)
 			topics = sp[1].split('/')
 			userData[uid] = topics
-	with open('question_info.txt', 'r') as f1:
+	with open('../train_data/question_info.txt', 'r') as f1:
 		for line in f1:
 			sp = line.split()
 			qid = sp[0]
 			topic = sp[1]
 			questionData[qid] = topic
-	with open('invited_info_train.txt', 'r') as f1:
+	with open('../train_data/invited_info_train.txt', 'r') as f1:
 		for line in f1:
 			line = line.rstrip('\n')
 			sp = line.split()
 			trainData.append((sp[0], sp[1], int(sp[2])))
-	with open('validate_nolabel.txt', 'r') as f1:
+	with open('../train_data/validate_nolabel.txt', 'r') as f1:
 		for line in f1:
 			valData.append(line.rstrip('\r\n').split(','))
 	#wv = pickle.load(open('user_word_wordvec.p', 'rb'))
 	#for i in range(0, len(wv)):
 		#wordvec[userorder[i]] = wv[i]
 
-
-
 	return userData, questionData, trainData, valData[1:], wordvec
+
+
 
 K=100
 userData, questionData, trainData, valData, wordvec = loadData()
-qList = questionData.keys()
-userProfiles = getUserProfiles(userData, questionData, trainData, qList, wordvec)
-nb, uList = getKNearestUsers(userProfiles, K)
+# qList = questionData.keys()
+# userProfiles = getUserProfiles(userData, questionData, trainData, qList, wordvec)
+user_keys = userData.keys()
+itemProfiles = getItemProfiles(userData, questionData, trainData, user_keys, wordvec)
+
+nb, uList = getKNearestUsers(itemProfiles, K)
 #pdb.set_trace()
 
-scores = getScores(nb, uList, userProfiles, valData, qList)
+scores = getScores(nb, uList, itemProfiles, valData, user_keys)
 umax = float("-inf")
 umin = float("inf")
 
@@ -145,8 +175,8 @@ for score in scores:
 	umax = max(umax, score[2])
 	umin = min(umin, score[2])
 
-pdb.set_trace()
-with open('v_userfilter_all'+str(K)+'.csv', 'w') as f1:
+# pdb.set_trace()
+with open('../validation/v_userfilter_all'+str(K)+'.csv', 'w') as f1:
 	f1.write('qid,uid,label\n')
 	for score in scores:
 		diff = umax - umin
