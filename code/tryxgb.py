23133deada6bb299c5e5b6a5dfddad99c938e170
Jashwanth Reddy Madem <jswt001@gmail.com>
2016-11-15 12:19:42 -0800

    xgboost

--- code/tryxgb.py
+++ code/tryxgb.py
@@ -6,29 +6,45 @@ import cPickle as pickle
 ## data
 question_feats = {}
 question_keys = pickle.load(open('../features/question_info_keys.dat', 'rb'))
-tf1 = pickle.load(open('../features/ques_charid_tfidf.dat', 'rb'))
-tf1_x = tf1.toarray()
-for i in range(len(tf1_x)):
-	question_feats[question_keys[i]] = tf1_x[i]
+ques_charid = pickle.load(open('../features/ques_charid_tfidf.dat', 'rb'))
+ques_wordid = pickle.load(open('../features/ques_wordid_tfidf.dat', 'rb'))
+ques_tags = pickle.load(open('../features/ques_tags.dat', 'rb'))
+ques_tags = ques_tags.toarray()
+ques_charid = ques_charid.toarray()
+ques_wordid = ques_wordid.toarray()
+for i in range(len(ques_charid)):
+	question_feats[question_keys[i]] = [ques_tags[i], ques_charid[i], ques_wordid[i]]
 
 user_feats = {}
 user_keys = pickle.load(open('../features/user_info_keys.dat', 'rb'))
-tf2 = pickle.load(open('../features/user_charid_tfidf.dat', 'rb'))
-tf2_x = tf2.toarray()
-for i in range(len(tf2_x)):
-	user_feats[user_keys[i]] = tf2_x[i]
+user_charid = pickle.load(open('../features/user_charid_tfidf.dat', 'rb'))
+user_wordid = pickle.load(open('../features/user_wordid_tfidf.dat', 'rb'))
+user_tags = pickle.load(open('../features/user_tags.dat', 'rb'))
+user_tags = user_tags.toarray()
+user_charid = user_charid.toarray()
+user_wordid = user_wordid.toarray()
+for i in range(len(user_charid)):
+	user_feats[user_keys[i]] = [user_tags[i], user_charid[i], user_wordid[i]]
+
+
 
 # Train data
 with open('../train_data/invited_info_train.txt') as train_file:
 	content = train_file.readlines()
 
 element = content[0].strip("\n").split("\t")
-data = np.zeros(shape=(len(content), len(question_feats[element[0]])+len(user_feats[element[1]])))
+no_feats = len(question_feats[element[0]][0])+len(user_feats[element[1]][0]) 
+			+ len(question_feats[element[0]][1])+len(user_feats[element[1]][1])
+			+ len(question_feats[element[0]][2])+len(user_feats[element[1]][2])
+
+data = np.zeros(shape=(len(content), no_feats))
 label = np.zeros(shape=(len(content),1))
 
 for i in range(len(content)):
 	element = content[i].strip("\n").split("\t")
-	data[i] = np.hstack((question_feats[element[0]], user_feats[element[1]]))
+	data[i] = np.hstack((question_feats[element[0]][0], user_feats[element[1]][0], 
+						question_feats[element[0]][1], user_feats[element[1]][1],
+						question_feats[element[0]][2], user_feats[element[1]][2]))
 	label[i]= element[2]
 
 
@@ -48,49 +64,32 @@ with open('../train_data/validate_nolabel.txt') as train_file:
 	content = train_file.readlines()
 testData = []
 element = content[1].strip("\r\n").split(",")
-data = np.zeros(shape=(len(content)-1, len(question_feats[element[0]])+len(user_feats[element[1]])))
+no_feats = len(question_feats[element[0]][0])+len(user_feats[element[1]][0]) 
+			+ len(question_feats[element[0]][1])+len(user_feats[element[1]][1])
+			+ len(question_feats[element[0]][2])+len(user_feats[element[1]][2])
+data = np.zeros(shape=(len(content)-1, no_feats))
 for i in range(1, len(content)):
 	element = content[i].strip("\r\n").split(",")
 	testData.append(element)
-	data[i-1] = np.hstack((question_feats[element[0]], user_feats[element[1]]))
+	data[i-1] = np.hstack((question_feats[element[0]][0], user_feats[element[1]][0], 
+						question_feats[element[0]][1], user_feats[element[1]][1],
+						question_feats[element[0]][2], user_feats[element[1]][2]))
 
 print data.shape
 dtest = xgb.DMatrix(data)
 
-##########################################################
 
-# To load a scpiy.sparse array into DMatrix, the command is:
-# csr = scipy.sparse.csr_matrix((dat, (row, col)))
-# dtrain = xgb.DMatrix(csr)
 
 
 # Booster parameters
-param = {'objective':'rank:pairwise', 'max_depth':'20', 'eval_metric':'ndcg@10000', 'eta':'0.27' }
-# param['nthread'] = 4
-# param['eval_metric'] = 'auc'
-# You can also specify multiple eval metrics:
-# param['eval_metric'] = ['auc', 'ams@0'] 
-
-# # alternativly:
-# # plst = param.items()
-# # plst += [('eval_metric', 'ams@0')]
-# Specify validations set to watch performance
-# evallist  = [(dtest,'eval'), (dtrain,'train')]
-
-
-
-# With parameter list and data, you are able to train a model.
+param = {'objective':'rank:pairwise', 'max_depth':'20', 'eval_metric':'ndcg@20000', 'eta':'0.18' }
 
 # Training
-num_round = 100
+num_round = 630
 bst = xgb.train(param, dtrain, num_round, evallist)
 
 # Prediction
-# # 7 entities, each contains 10 features
-# data = np.random.rand(7, 10)
-# dtest = xgb.DMatrix(data)
-
-ypred = bst.predict(dtest)
+ypred = bst.predict(dtest, ntree_limit=bst.best_ntree_limit)
 # If early stopping is enabled during training, you can get predicticions from the best iteration with bst.best_ntree_limit:
 
 # ypred = bst.predict(xgmat,ntree_limit=bst.best_ntree_limit)
